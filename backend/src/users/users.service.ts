58430d79be6387099f9ddead95debf0244d8c252
001kirillka-jpg <001kirillka@gmail.com>
2026-03-30 23:19:46 +0300

    Add users service

---
+++ backend/src/users/users.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../database/prisma.service';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(data: { login: string; password: string }) {
+    const hash = await bcrypt.hash(data.password, 10);
+
+    return this.prisma.user.create({
+      data: {
+        login: data.login,
+        password: hash,
+      },
+    });
+  }
+
+  async findAll() {
+    return this.prisma.user.findMany({
+      include: {
+        roles: {
+          include: {
+            role: true,
+          },
+        },
+      },
+    });
+  }
+}
